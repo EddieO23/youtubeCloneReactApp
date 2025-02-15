9e2c5470eef96066d271c94ce61494b25e8b0dc9
Eddie Ocampo <eddie@Eddies-MacBook-Air.local>
2025-02-15 15:24:47 -0600

    added useHome hook to app. Working on pagToken Youtube API. Lecture 26

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,71 +2,13 @@ import React, { useEffect, useState } from 'react';
 import Card from '../Components/Card';
 import axios from 'axios';
 // import { homeVideoCad } from '../utils/Type';
+import { useHome } from '../Hooks/useHome';
 
-const API_KEY = import.meta.env.VITE_API_KEY;
-
-function Home({categoryId, filter}) {
-  const [homeVideos, setHomeVideos] = useState([]);
-
-  const fetchHomeVideos = async () => {
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&part=snippet,statistics,contentDetails&chart=mostPopular&${categoryId != null ? `videoCategoryId=${categoryId}` : ``}&maxResults=20`
-      );
-      console.log(response.data);
-
-      const videoData = response.data.items.map((item) => {
-        return {
-          videoId: item.id,
-          videoTitle: item.snippet.title,
-          videoThumbnail: item.snippet.thumbnails.standard.url,
-          videoDuration: item.contentDetails.duration,
-          videoViews: item.statistics.viewCount,
-          videoAge: item.snippet.publishedAt,
-          channelInfo: {
-            id: item.snippet.channelId,
-            name: item.snippet.channelTitle,
-          },
-        };
-      });
-
-      const channelIds = videoData
-        .map((video) => video.channelInfo.id)
-        .join(',');
-
-      const channelResponse = await axios.get(
-        `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet&id=${channelIds}`
-      );
-
-      // console.log(channelResponse);
-
-      const channelData = {};
-
-      channelResponse.data.items.forEach((channel) => {
-        channelData[channel.id] = {
-          id: channel.id,
-          image: channel.snippet.thumbnails.default.url,
-        };
-      });
-
-      // console.log(channelData);
-
-      const videos = videoData.map((video) => ({
-        ...video,
-        channelInfo: {
-          ...video.channelInfo,
-          image: channelData[video.channelInfo.id].image,
-        },
-      }));
-
-      setHomeVideos(videos);
-    } catch (error) {
-      console.error(`Error fetching: ${filter} videos"`, error);
-    }
-  };
+function Home({ categoryId, filter }) {
+  const { homeVideos, fetchHomeVideos } = useHome();
 
   useEffect(() => {
-    fetchHomeVideos();
+    fetchHomeVideos(filter, categoryId);
   }, [categoryId]);
 
   useEffect(() => {

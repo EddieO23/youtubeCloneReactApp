3f223e1120250dba8ba0723d5136c5858a3b62aa
Eddie Ocampo <eddie@Eddies-MacBook-Air.local>
2025-02-15 16:01:05 -0600

    debug categories not rendering on page. lecture 25. working on pagetoken for app. added new useHome hook

--- src/Hooks/useHome.js
+++ src/Hooks/useHome.js
@@ -1,68 +1,66 @@
-import { useState } from 'react';
-import axios from 'axios';
+import { useState } from "react";
+import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const useHome = () => {
-  const [homeVideos, setHomeVideos] = useState([]);
-
-  const fetchHomeVideos = async (filter, categoryId) => {
-    try {
-      const response = await axios.get(
-        `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&part=snippet,statistics,contentDetails&chart=mostPopular&${
-          categoryId != null ? `videoCategoryId=${categoryId}` : ``
-        }&maxResults=20`
-      );
-      console.log(response.data);
-
-      const videoData = response.data.items.map((item) => {
-        return {
-          videoId: item.id,
-          videoTitle: item.snippet.title,
-          videoThumbnail: item.snippet.thumbnails.standard.url,
-          videoDuration: item.contentDetails.duration,
-          videoViews: item.statistics.viewCount,
-          videoAge: item.snippet.publishedAt,
+    const [homeVideos, setHomeVideos] = useState([]);
+  
+    const fetchHomeVideos = async ({filter, categoryId}) => {
+      try {
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&part=snippet,statistics,contentDetails&chart=mostPopular&${categoryId != null ? `videoCategoryId=${categoryId}` : ``}&maxResults=20`
+        );
+        console.log(response.data);
+  
+        const videoData = response.data.items.map((item) => {
+          return {
+            videoId: item.id,
+            videoTitle: item.snippet.title,
+            videoThumbnail: item.snippet.thumbnails.standard.url,
+            videoDuration: item.contentDetails.duration,
+            videoViews: item.statistics.viewCount,
+            videoAge: item.snippet.publishedAt,
+            channelInfo: {
+              id: item.snippet.channelId,
+              name: item.snippet.channelTitle,
+            },
+          };
+        });
+  
+        const channelIds = videoData
+          .map((video) => video.channelInfo.id)
+          .join(',');
+  
+        const channelResponse = await axios.get(
+          `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet&id=${channelIds}`
+        );
+  
+        // console.log(channelResponse);
+  
+        const channelData = {};
+  
+        channelResponse.data.items.forEach((channel) => {
+          channelData[channel.id] = {
+            id: channel.id,
+            image: channel.snippet.thumbnails.default.url,
+          };
+        });
+  
+        // console.log(channelData);
+  
+        const videos = videoData.map((video) => ({
+          ...video,
           channelInfo: {
-            id: item.snippet.channelId,
-            name: item.snippet.channelTitle,
+            ...video.channelInfo,
+            image: channelData[video.channelInfo.id].image,
           },
-        };
-      });
-
-      const channelIds = videoData
-        .map((video) => video.channelInfo.id)
-        .join(',');
-
-      const channelResponse = await axios.get(
-        `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet&id=${channelIds}`
-      );
-
-      // console.log(channelResponse);
-
-      const channelData = {};
-
-      channelResponse.data.items.forEach((channel) => {
-        channelData[channel.id] = {
-          id: channel.id,
-          image: channel.snippet.thumbnails.default.url,
-        };
-      });
-
-      // console.log(channelData);
-
-      const videos = videoData.map((video) => ({
-        ...video,
-        channelInfo: {
-          ...video.channelInfo,
-          image: channelData[video.channelInfo.id].image,
-        },
-      }));
-
-      setHomeVideos(videos);
-    } catch (error) {
-      console.error(`Error fetching: ${filter} videos"`, error);
-    }
-  };
-  return { homeVideos, fetchHomeVideos };
-};
+        }));
+  
+        setHomeVideos(videos);
+      } catch (error) {
+        console.error(`Error fetching: ${filter} videos"`, error);
+      }
+    };
+    return {homeVideos, fetchHomeVideos}
+}
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,8 +4,67 @@ import axios from 'axios';
 // import { homeVideoCad } from '../utils/Type';
 import { useHome } from '../Hooks/useHome';
 
-function Home({ categoryId, filter }) {
-  const { homeVideos, fetchHomeVideos } = useHome();
+
+function Home({categoryId, filter}) {
+
+  // const fetchHomeVideos = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://www.googleapis.com/youtube/v3/videos?key=${API_KEY}&part=snippet,statistics,contentDetails&chart=mostPopular&${categoryId != null ? `videoCategoryId=${categoryId}` : ``}&maxResults=20`
+  //     );
+  //     console.log(response.data);
+
+  //     const videoData = response.data.items.map((item) => {
+  //       return {
+  //         videoId: item.id,
+  //         videoTitle: item.snippet.title,
+  //         videoThumbnail: item.snippet.thumbnails.standard.url,
+  //         videoDuration: item.contentDetails.duration,
+  //         videoViews: item.statistics.viewCount,
+  //         videoAge: item.snippet.publishedAt,
+  //         channelInfo: {
+  //           id: item.snippet.channelId,
+  //           name: item.snippet.channelTitle,
+  //         },
+  //       };
+  //     });
+
+  //     const channelIds = videoData
+  //       .map((video) => video.channelInfo.id)
+  //       .join(',');
+
+  //     const channelResponse = await axios.get(
+  //       `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet&id=${channelIds}`
+  //     );
+
+  //     // console.log(channelResponse);
+
+  //     const channelData = {};
+
+  //     channelResponse.data.items.forEach((channel) => {
+  //       channelData[channel.id] = {
+  //         id: channel.id,
+  //         image: channel.snippet.thumbnails.default.url,
+  //       };
+  //     });
+
+  //     // console.log(channelData);
+
+  //     const videos = videoData.map((video) => ({
+  //       ...video,
+  //       channelInfo: {
+  //         ...video.channelInfo,
+  //         image: channelData[video.channelInfo.id].image,
+  //       },
+  //     }));
+
+  //     setHomeVideos(videos);
+  //   } catch (error) {
+  //     console.error(`Error fetching: ${filter} videos"`, error);
+  //   }
+  // };
+
+  const {homeVideos, fetchHomeVideos} = useHome()
 
   useEffect(() => {
     fetchHomeVideos(filter, categoryId);

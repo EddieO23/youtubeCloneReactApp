706f8c8748619f40533b29cdb2248a492f6f3b8f
Eddie <eocampo52@gmail.com>
2025-02-16 16:49:31 -0600

    working on Watch page. Added Watch component. Added VideoDetails component. Currently working on creating the template then will be adding dynamic data according to YOUTUBEAPI. Added the share and like icons. Added Basic Template layout to app. Left off on working mini cards layout for recommended videos

--- src/Pages/Watch.jsx
+++ src/Pages/Watch.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
+import VideoDetails from '../Components/VideoDetails';
 
 function Watch() {
   return (
-    <div>
-      WATCH
+    <div className='w-[95%] mx-auto mt-6'>
+      <div className='row'>
+        <div className='col-8'>
+          <div className='w-full aspect-[16/9] bg-red-400'></div>
+          <VideoDetails/>
+          <div className="">1</div>
+        </div>
+        <div className='col-4'>2</div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Watch
+export default Watch;

1eaf331fd5900706e2d7ff51f3b57c62b32b12fb
Eddie <eocampo52@gmail.com>
2025-02-20 15:43:56 -0600

    Added Channel component to push

---
+++ src/Pages/Channel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Channel() {
+  return (
+    <div className='w-[95%] mx-auto'>
+      <div className="row row-cols-2">
+        {/* image */}
+        <div className="col-4">
+          <div className="w-52 aspect-[1/1] rounded-full bg-blue-300"></div>
+        </div>
+        {/* details */}
+      </div>
+    </div>
+  )
+}
+
+export default Channel

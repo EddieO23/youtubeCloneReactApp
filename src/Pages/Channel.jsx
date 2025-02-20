9619bad9ab54d43a449a3ec57ec640f93be97ebe
Eddie <eocampo52@gmail.com>
2025-02-20 17:23:52 -0600

    succesfully rendering dynamic channelData to channel page using youtube API

--- src/Pages/Channel.jsx
+++ src/Pages/Channel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
@@ -7,12 +7,27 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Channel() {
   const { channelId } = useParams();
+  const [channelInfo, setChannelInfo] = useState(null);
 
   const fetchChannelInfo = async () => {
     const channelInfoResponse = await axios.get(
       `https://www.googleapis.com/youtube/v3/channels?key=${API_KEY}&part=snippet,contentDetails,statistics&id=${channelId}`
     );
     console.log('channelResponse', channelInfoResponse);
+
+    const items = channelInfoResponse.data.items;
+
+    const channelInfoData = items.map((item) => ({
+      id: item.id,
+      thumbnail: item.snippet.thumbnails.high.url,
+      title: item.snippet.title,
+      customUrl: item.snippet.customUrl,
+      description: item.snippet.description,
+      subCount: item.statistics.subscriberCount,
+      videoCount: item.statistics.videoCount,
+    }));
+    // console.log('channelInfoData response: ', channelInfoData);
+    setChannelInfo(channelInfoData[0]);
   };
 
   useEffect(() => {
@@ -24,24 +39,27 @@ function Channel() {
       <div className='row row-cols-2'>
         {/* image */}
         <div className='col-4'>
-          <div className='w-52 aspect-[1/1] rounded-full bg-red-300'></div>
+          {/* <div className='w-52 aspect-[1/1] rounded-full bg-red-300'></div> */}
+          
+           <img src={channelInfo?.thumbnail} className='w-52 aspect-[1/1] object-cover rounded-full' alt="channelThumbnail img" />
         </div>
         {/* details */}
         <div className='col-8'>
-          <h1 className='text-4xl font-semibold'>Channel Title</h1>
+          <h1 className='text-4xl font-semibold'>{channelInfo?.title}</h1>
           <div className='flex gap-4 text-lg mt-2 text-neutral-400'>
-            <h3>Custom Url</h3>
-            <h3>Sub Count</h3>
-            <h3>Video Count</h3>
+            <h3>{channelInfo?.customUrl}</h3>
+            <h3>{channelInfo?.subCount} Subscribers</h3>
+            <h3>{channelInfo?.videoCount} Videos</h3>
           </div>
 
           {/* Description */}
           <div className=''>
             <p className='w-[600px] line-clamp-3 text-neutral-400 whitespace-pre-line'>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
+              {/* Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
               placeat deleniti repellendus officia dolorum optio, quibusdam vel,
               rerum eligendi porro doloribus pariatur culpa itaque dolores amet
-              delectus molestias velit asperiores.
+              delectus molestias velit asperiores. */}
+              {channelInfo?.description}
             </p>
             <button className='font-semibold'>more</button>
           </div>

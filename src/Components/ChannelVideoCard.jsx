157f185392bcea497b4bd1e4e6e8404bce84648f
Eddie <eocampo52@gmail.com>
2025-02-21 21:04:47 -0600

    added new classes to dynamic data on channel page

--- src/Components/ChannelVideoCard.jsx
+++ src/Components/ChannelVideoCard.jsx
@@ -13,10 +13,10 @@ function ChannelVIdeoCard({item}) {
       </div>
       {/* title */}
       <div className='flex flex-col gap-1 mt-1 '>
-        <h2 className='text-md line-clamp-1'>{item.videoTitle}</h2>
+        <h3 className='text-md line-clamp-1'>{item.videoTitle}</h3>
         <div className='flex gap-3 text-sm text-gray-400'>
-          <h3>{item.videoViews}</h3>
-          <h3>{item.videoAge}</h3>
+          <h4>{item.videoViews}</h4>
+          <h4>{item.videoAge}</h4>
         </div>
       </div>
     </div>
